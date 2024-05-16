e4bff13f14af72c064bb55fb478e2dfff9784ed6
k.spiropoulos <github@ai2cyber.com>
2024-05-16 14:20:16 +0300

    feat: Add examples

---
+++ examples/polymorphism_support.py
@@ -0,0 +1,23 @@
+from typing import List
+from uuid import UUID, uuid4
+
+from pyvnt import Entity, descriptor
+from pyvnt.transformers.common import uuid_transformer
+from pyvnt.transformers.sequence import list_transformer
+
+
+class Sample(Entity):
+    property_one: List[UUID] = descriptor(transformations=[list_transformer(uuid_transformer())])
+
+
+class InheritedSample(Entity):
+    property_two: int = descriptor()
+
+
+uuids = [uuid4(), uuid4(), uuid4()]
+sample = Sample(uuids)
+inherited_sample = InheritedSample(10)
+serialized_inherited_sample = inherited_sample.serialize()
+
+assert inherited_sample == Sample.deserialize(serialized_inherited_sample)
+# Notice that you called the deserialize method from the Sample class and yet it returned an Inherited sample instance.
